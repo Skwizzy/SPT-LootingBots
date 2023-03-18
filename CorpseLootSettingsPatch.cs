0723b7e9646ca87c900dcb687203616071fc6a30
skwizzy <=>
2023-03-18 02:53:49 -0500

    Default pmc looting to false until wep logic enhanced

--- CorpseLootSettingsPatch.cs
+++ CorpseLootSettingsPatch.cs
@@ -19,34 +19,38 @@ namespace LootingBots.Patch
             ref WildSpawnType ___wildSpawnType_0
         )
         {
+            // Unchecked to get around cast of usec/bear WildSpawnType added in AkiBotsPrePatcher
+            unchecked
+            {
+                WildSpawnType bear = (WildSpawnType)Aki.PrePatch.AkiBotsPrePatcher.sptBearValue;
+                WildSpawnType usec = (WildSpawnType)Aki.PrePatch.AkiBotsPrePatcher.sptUsecValue;
+
+                bool isPMC = ___wildSpawnType_0 == bear || ___wildSpawnType_0 == usec;
+
+                if ((isPMC) && LootingBots.pmcLootingEnabled.Value)
+                {
+                    Logger.LogDebug("Setting config for PMC");
+                    enableLooting(__instance);
+                }
+                else if (!isPMC)
+                {
+                    Logger.LogDebug("Setting config for bot");
+                    enableLooting(__instance);
+                }
+            }
+        }
+
+        public static void enableLooting(BotDifficultySettingsClass __instance)
+        {
             float seeDist = LootingBots.bodySeeDist.Value;
             float leaveDist = LootingBots.bodyLeaveDist.Value;
             float lookPeriod = LootingBots.bodyLookPeriod.Value;
-            unchecked {
-            WildSpawnType bear = (WildSpawnType)Aki.PrePatch.AkiBotsPrePatcher.sptBearValue;
-            WildSpawnType usec = (WildSpawnType)Aki.PrePatch.AkiBotsPrePatcher.sptUsecValue;
-
-            bool isPMC = ___wildSpawnType_0 == bear || ___wildSpawnType_0 == usec;
-
-            if ((isPMC) && LootingBots.pmcLootingEnabled.Value)
-            {
-                Logger.LogDebug("Setting config for PMC");
-                __instance.FileSettings.Patrol.CAN_LOOK_TO_DEADBODIES = true;
-                __instance.FileSettings.Mind.HOW_WORK_OVER_DEAD_BODY = 2;
-                __instance.FileSettings.Patrol.DEAD_BODY_SEE_DIST = seeDist;
-                __instance.FileSettings.Patrol.DEAD_BODY_LEAVE_DIST = leaveDist;
-                __instance.FileSettings.Patrol.DEAD_BODY_LOOK_PERIOD = lookPeriod;
-            }
-            else if (!isPMC)
-            {
-                Logger.LogDebug("Setting config for bot");
-                __instance.FileSettings.Patrol.CAN_LOOK_TO_DEADBODIES = true;
-                __instance.FileSettings.Mind.HOW_WORK_OVER_DEAD_BODY = 2;
-                __instance.FileSettings.Patrol.DEAD_BODY_SEE_DIST = seeDist;
-                __instance.FileSettings.Patrol.DEAD_BODY_LEAVE_DIST = leaveDist;
-                __instance.FileSettings.Patrol.DEAD_BODY_LOOK_PERIOD = lookPeriod;
-            }
-            }
+            
+            __instance.FileSettings.Patrol.CAN_LOOK_TO_DEADBODIES = true;
+            __instance.FileSettings.Mind.HOW_WORK_OVER_DEAD_BODY = 2;
+            __instance.FileSettings.Patrol.DEAD_BODY_SEE_DIST = seeDist;
+            __instance.FileSettings.Patrol.DEAD_BODY_LEAVE_DIST = leaveDist;
+            __instance.FileSettings.Patrol.DEAD_BODY_LOOK_PERIOD = lookPeriod;
         }
     }
 }
