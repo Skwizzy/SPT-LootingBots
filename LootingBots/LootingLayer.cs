316f128aaeeb9a9cfc0d77c1aa5fab20a09ec8fe
Skwizzy <thedefectiverobot@gmail.com>
2023-07-21 22:29:43 -0500

    Change debug menu to show calculating path...

--- LootingBots/LootingLayer.cs
+++ LootingBots/LootingLayer.cs
@@ -118,7 +118,7 @@ namespace LootingBots.Brain
 
             debugPanel.AppendLabeledValue(
                 $"Distance to Loot",
-                $" {(category == "" || _lootingBrain.DistanceToLoot == -1f ? "-" : Math.Sqrt(_lootingBrain.DistanceToLoot).ToString("0.##"))}m",
+                $" {(category == "" || _lootingBrain.DistanceToLoot == -1f ? "Calculating path..." : $"{Math.Sqrt(_lootingBrain.DistanceToLoot):0.##}m")}",
                 Color.grey,
                 Color.grey
             );
