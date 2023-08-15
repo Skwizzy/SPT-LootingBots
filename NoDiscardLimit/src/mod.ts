ed992197df13d437bc0051d0cf0cd691f306a798
space-commits <57108281+space-commits@users.noreply.github.com>
2023-08-15 17:47:08 +0100

    Remove loot from bots
    Added changes to server mod to allow the removal spawned loot from bots.

--- NoDiscardLimit/src/mod.ts
+++ NoDiscardLimit/src/mod.ts
@@ -3,14 +3,53 @@ import { DependencyContainer } from "tsyringe";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { ConfigServer } from "@spt-aki/servers/ConfigServer";
+import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
+import { IBotConfig } from "@spt-aki/models/spt/config/IBotConfig";
+
+const config = require("../config/config.json");
+import { ParentClasses } from "./enums.js";
 
 class DisableDiscardLimits implements IPostDBLoadMod {
   public postDBLoad(container: DependencyContainer): void {
     const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
     const logger = container.resolve<ILogger>("WinstonLogger");
+    const configServer = container.resolve<ConfigServer>("ConfigServer");
+    const botConf = configServer.getConfig<IBotConfig>(ConfigTypes.BOT);
 
-    const tables = databaseServer.getTables()
-    
+    const tables = databaseServer.getTables();
+
+    const allowedItemTypes: string[] = [ParentClasses.THROW_WEAPON, ParentClasses.AMMO, ParentClasses.MEDICAL, ParentClasses.MEDKIT, ParentClasses.DRUGS, ParentClasses.DRINK, ParentClasses.FOOD, ParentClasses.FOOD_DRINK, ParentClasses.STIMULATOR];
+
+    for (let i in config.bot_types) {
+      let botType = config.bot_types[i];
+      if (config.empty_pockets) {
+        tables.bots.types[botType].inventory.items.Pockets = [];
+      }
+      if (config.empty_bag) {
+        tables.bots.types[botType].inventory.items.Backpack = [];
+      }
+    }
+
+    botConf.pmc.looseWeaponInBackpackLootMinMax.min = 0;
+    botConf.pmc.looseWeaponInBackpackLootMinMax.max = 0;
+
+    //have to add all loot items we don't want to pmc blacklist because PMCs use "dynamic loot" pool
+    for (let item in tables.templates.items) {
+      let serverItem = tables.templates.items[item];
+      for (let allowedItem in allowedItemTypes) {
+        if (serverItem._parent !== allowedItemTypes[allowedItem]) {
+          if (config.empty_pockets) {
+            botConf.pmc.pocketLoot.blacklist.push(serverItem._id);
+          }
+          if (config.empty_bag) {
+            botConf.pmc.backpackLoot.blacklist.push(serverItem._id);
+          }
+          botConf.pmc.vestLoot.blacklist.push(serverItem._id);
+        }
+      }
+    }
+
     logger.info("Marking items with DiscardLimits as InsuranceDisabled")
     for (let itemId in tables.templates.items) {
       const template = tables.templates.items[itemId];
