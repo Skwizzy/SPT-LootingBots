81ee300611a2d9554044b7908387926494eb302a
Skwizzy <thedefectiverobot@gmail.com>
2023-08-16 18:56:52 -0500

    add build changes

--- Build/user/mods/Skwizzy-NoDiscardLimit/src/mod.ts
+++ Build/user/mods/Skwizzy-NoDiscardLimit/src/mod.ts
@@ -3,14 +3,55 @@ import { DependencyContainer } from "tsyringe";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { ConfigServer } from "@spt-aki/servers/ConfigServer";
+import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
+import { IBotConfig } from "@spt-aki/models/spt/config/IBotConfig";
+
+const config = require("../config/config.json");
+import { ParentClasses } from "./enums.js";
 
 class DisableDiscardLimits implements IPostDBLoadMod {
   public postDBLoad(container: DependencyContainer): void {
     const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
     const logger = container.resolve<ILogger>("WinstonLogger");
+    const configServer = container.resolve<ConfigServer>("ConfigServer");
+    const botConf = configServer.getConfig<IBotConfig>(ConfigTypes.BOT);
 
-    const tables = databaseServer.getTables()
-    
+    const tables = databaseServer.getTables();
+
+    const allowedItemTypes: string[] = [ParentClasses.THROW_WEAPON, ParentClasses.AMMO, ParentClasses.MEDICAL, ParentClasses.MEDKIT, ParentClasses.DRUGS, ParentClasses.DRINK, ParentClasses.FOOD, ParentClasses.FOOD_DRINK, ParentClasses.STIMULATOR];
+    const pmcConfig = botConf.pmc;
+
+    for (let i in config.bot_types) {
+      let botType = config.bot_types[i];
+      if (config.empty_pockets) {
+        logger.info("Emptying PMC pockets");
+        tables.bots.types[botType].inventory.items.Pockets = [];
+      }
+      if (config.empty_bag) {
+        logger.info("Emptying PMC backpacks");
+        tables.bots.types[botType].inventory.items.Backpack = [];
+      }
+    }
+
+    pmcConfig.looseWeaponInBackpackLootMinMax.min = 0;
+    pmcConfig.looseWeaponInBackpackLootMinMax.max = 0;
+
+    //have to add all loot items we don't want to pmc blacklist because PMCs use "dynamic loot" pool
+    for (let item in tables.templates.items) {
+      let serverItem = tables.templates.items[item];
+      if (!allowedItemTypes.includes(serverItem._parent)) {
+        if (config.empty_pockets) {
+          pmcConfig.pocketLoot.blacklist.push(serverItem._id);
+        }
+        if (config.empty_bag) {
+          pmcConfig.backpackLoot.blacklist.push(serverItem._id);
+        }
+
+        pmcConfig.vestLoot.blacklist.push(serverItem._id);
+      }
+    }
+
     logger.info("Marking items with DiscardLimits as InsuranceDisabled")
     for (let itemId in tables.templates.items) {
       const template = tables.templates.items[itemId];
