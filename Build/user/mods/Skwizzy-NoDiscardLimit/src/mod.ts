a8a2c7489a80eeb627b1cfa102691ac060a41cd1
Skwizzy <thedefectiverobot@gmail.com>
2023-08-16 19:24:50 -0500

    Run prettier formatting

--- Build/user/mods/Skwizzy-NoDiscardLimit/src/mod.ts
+++ Build/user/mods/Skwizzy-NoDiscardLimit/src/mod.ts
@@ -13,23 +13,33 @@ import { ParentClasses } from "./enums.js";
 class DisableDiscardLimits implements IPostDBLoadMod {
   public postDBLoad(container: DependencyContainer): void {
     const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-    const logger = container.resolve<ILogger>("WinstonLogger");
     const configServer = container.resolve<ConfigServer>("ConfigServer");
     const botConf = configServer.getConfig<IBotConfig>(ConfigTypes.BOT);
+    const { logInfo } = useLogger(container);
 
     const tables = databaseServer.getTables();
 
-    const allowedItemTypes: string[] = [ParentClasses.THROW_WEAPON, ParentClasses.AMMO, ParentClasses.MEDICAL, ParentClasses.MEDKIT, ParentClasses.DRUGS, ParentClasses.DRINK, ParentClasses.FOOD, ParentClasses.FOOD_DRINK, ParentClasses.STIMULATOR];
+    const allowedItemTypes: string[] = [
+      ParentClasses.THROW_WEAPON,
+      ParentClasses.AMMO,
+      ParentClasses.MEDICAL,
+      ParentClasses.MEDKIT,
+      ParentClasses.DRUGS,
+      ParentClasses.DRINK,
+      ParentClasses.FOOD,
+      ParentClasses.FOOD_DRINK,
+      ParentClasses.STIMULATOR,
+    ];
     const pmcConfig = botConf.pmc;
 
     for (let i in config.bot_types) {
       let botType = config.bot_types[i];
       if (config.empty_pockets) {
-        logger.info(`Emptying ${botType} pockets`);
+        logInfo(`Emptying ${botType} pockets`);
         tables.bots.types[botType].inventory.items.Pockets = [];
       }
       if (config.empty_bag) {
-        logger.info(`Emptying ${botType} backpacks`);
+        logInfo(`Emptying ${botType} backpacks`);
         tables.bots.types[botType].inventory.items.Backpack = [];
       }
     }
@@ -52,20 +62,29 @@ class DisableDiscardLimits implements IPostDBLoadMod {
       }
     }
 
-    logger.info("Marking items with DiscardLimits as InsuranceDisabled")
+    logInfo("Marking items with DiscardLimits as InsuranceDisabled");
     for (let itemId in tables.templates.items) {
       const template = tables.templates.items[itemId];
       // When we set DiscardLimitsEnabled to false further down, this will cause some items to be able to be insured when they normally should not be.
       // The DiscardLimit property is used by BSG for RMT protections and their code internally treats things with discard limits as not insurable.
-      // For items that have a DiscardLimit >= 0, we need to manually flag them as InsuranceDisabled to make sure they still cannot be insured by the player. 
+      // For items that have a DiscardLimit >= 0, we need to manually flag them as InsuranceDisabled to make sure they still cannot be insured by the player.
       if (template._props.DiscardLimit >= 0) {
         template._props.InsuranceDisabled = true;
       }
     }
 
     tables.globals.config.DiscardLimitsEnabled = false;
-    logger.info("Global config DiscardLimitsEnabled set to false");
+    logInfo("Global config DiscardLimitsEnabled set to false");
   }
+}
+
+function useLogger(container: DependencyContainer) {
+  const logger = container.resolve<ILogger>("WinstonLogger");
+  return {
+    logInfo: (message: string) => {
+      logger.info(`[NoDiscardLimit] ${message}`);
+    },
+  };
 }
 
 module.exports = { mod: new DisableDiscardLimits() };
