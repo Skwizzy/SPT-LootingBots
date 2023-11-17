7ec7b099a6abfb9f94f07ae367f006fd78a9f9c6
Skwizzy <thedefectiverobot@gmail.com>
2023-11-17 14:24:44 -0600

    manually stop coroutines when logic is interrupted

--- LootingBots/logics/FindLootLogic.cs
+++ LootingBots/logics/FindLootLogic.cs
@@ -11,21 +11,30 @@ namespace LootingBots.Brain.Logics
     {
         private readonly LootingBrain _lootingBrain;
         private readonly LootFinder _lootFinder;
+        readonly BotLog _log;
 
         public FindLootLogic(BotOwner botOwner)
             : base(botOwner)
         {
             _lootingBrain = botOwner.GetPlayer.gameObject.GetComponent<LootingBrain>();
             _lootFinder = botOwner.GetPlayer.gameObject.GetComponent<LootFinder>();
+            _log = new BotLog(LootingBots.LootLog, botOwner);
         }
 
         public override void Update()
         {
             // If the bot has more than the reserved amount of slots needed for ammo, trigger a loot scan
-            if (_lootingBrain.Stats.AvailableGridSpaces > LootUtils.RESERVED_SLOT_COUNT)
+            if (_lootingBrain.Stats.AvailableGridSpaces > LootUtils.RESERVED_SLOT_COUNT && !_lootFinder.IsScanRunning)
             {
-                _lootFinder.Start();
+                _lootFinder.BeginSearch();
             }
+        }
+
+        public override void Stop()
+        {
+            _lootFinder.IsScanRunning = false;
+            _lootFinder.StopAllCoroutines();
+            base.Stop();
         }
     }
 }
