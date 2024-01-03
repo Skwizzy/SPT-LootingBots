8d1772552bd806629df0d73a2630de6e832aebd4
dwesterwick <dwesterwick@yahoo.com>
2024-01-02 22:48:14 -0500

    Added interop framework and confirmed functionality with Questing Bots

---
+++ LootingBots/External.cs
@@ -0,0 +1,18 @@
+﻿using EFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LootingBots
+{
+    public static class External
+    {
+        public static bool ForceBotToLootNow(BotOwner bot)
+        {
+            
+            return true;
+        }
+    }
+}
