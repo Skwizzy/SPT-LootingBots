19f1c8cf8f03f4ed8a08e46658e6c7b24d562432
Skwizzy <thedefectiverobot@gmail.com>
2024-04-01 17:50:21 -0500

    change version

--- LootingBots/LootingBots.cs
+++ LootingBots/LootingBots.cs
@@ -22,7 +22,7 @@ namespace LootingBots
     {
         private const string MOD_GUID = "me.skwizzy.lootingbots";
         private const string MOD_NAME = "LootingBots";
-        private const string MOD_VERSION = "1.2.2";
+        private const string MOD_VERSION = "1.3.0";
 
         public const BotType SettingsDefaults =
             BotType.Scav | BotType.Pmc | BotType.PlayerScav | BotType.Raider;
