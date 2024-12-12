9975cc9cd181bc870d8256cdeaa7a2d2bb21b5c7
Archangel <jesse@archangel.wtf>
2024-12-12 17:44:20 +0100

    Update project as VS didn't want to load it

---
+++ LootingBots/patches/DisableBotLooting/ItemDropper_Activate_Patch.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using SPT.Reflection.Patching;
+
+namespace skwizzy.LootingBots.patches.DisableBotLooting
+{
+    internal class ItemDropper_Activate_Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BotItemDropper).GetMethod(nameof(BotItemDropper.Activate));
+        }
+
+        [PatchPrefix]
+        private static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+}
