be48380c7d3138b508e3837ef667d1faf60b6791
Archangel <jesse@archangel.wtf>
2025-08-01 18:02:39 +0200

    Update 4.0 branch

--- LootingBotsServerMod/ModMetadata.cs
+++ LootingBotsServerMod/ModMetadata.cs
@@ -4,17 +4,18 @@ namespace LootingBotsServerMod
 {
     public record ModMetadata : AbstractModMetadata
     {
-        public override string Name { get; set; } = "LootingBots-ServerMod";
-        public override string Author { get; set; } = "Skwizzy";
+        public override string ModGuid { get; init; } = "me.skwizzy.lootingbots_servermod";
+        public override string Name { get; init; } = "LootingBots-ServerMod";
+        public override string Author { get; init; } = "Skwizzy";
         public override List<string>? Contributors { get; set; }
-        public override string Version { get; set; } = "1.6.0";
-        public override string SptVersion { get; set; } = "4.0.0";
+        public override string Version { get; init; } = "1.6.0";
+        public override string SptVersion { get; init; } = "4.0.0";
         public override List<string>? LoadBefore { get; set; }
         public override List<string>? LoadAfter { get; set; }
         public override List<string>? Incompatibilities { get; set; }
         public override Dictionary<string, string>? ModDependencies { get; set; }
         public override string? Url { get; set; }
         public override bool? IsBundleMod { get; set; }
-        public override string? Licence { get; set; } = "MIT";
+        public override string License { get; init; } = "MIT";
     }
 }
